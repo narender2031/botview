28fb21b5a15920cb324b32325b8c64d2b77c7e10
narender <narender2031@gmail.com>
2018-11-22 12:14:32 +0530

    update

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module Botview
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
