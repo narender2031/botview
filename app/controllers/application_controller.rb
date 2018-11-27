c25ab944f9bd242a1a52bfdb9d3d6c5ea5210806
narender <narender2031@gmail.com>
2018-11-27 15:55:04 +0530

    update code with setup alignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,19 +7,19 @@ class ApplicationController < ActionController::Base
 
     private
     def user_not_authorized
-        flash[:alert] = "You are not authorized to perform this action."
-        redirect_to(request.referrer || root_path)
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to(request.referrer || root_path)
     end
     
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone_number])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone_number])
     end
 
     def after_sign_in_path_for(resource)
-        # Here you can write logic based on roles to return different after-sign-in paths
-        root_path
+      # Here you can write logic based on roles to return different after-sign-in paths
+      root_path
     end
 
     def current_or_guest_user
@@ -35,16 +35,16 @@ class ApplicationController < ActionController::Base
         guest_user
       end
     end
-  
-    # find guest_user object associated with the current session,
-    # creating one as needed
+
+      # find guest_user object associated with the current session,
+      # creating one as needed
     def guest_user(with_retry = true)
       # Cache the value the first time it's gotten.
       @cached_guest_user ||= User.find(session[:guest_user_id] ||= create_guest_user.id)
   
     rescue ActiveRecord::RecordNotFound # if session[:guest_user_id] invalid
-       session[:guest_user_id] = nil
-       guest_user if with_retry
+      session[:guest_user_id] = nil
+      guest_user if with_retry
     end
   
 
@@ -52,12 +52,12 @@ class ApplicationController < ActionController::Base
     # called (once) when the user logs in, insert any code your application needs
     # to hand off from guest_user to current_user.
     def logging_in
-        # For example:
-        # guest_comments = guest_user.comments.all
-        # guest_comments.each do |comment|
-        # comment.user_id = current_user.id
-        # comment.save!
-        # end
+      # For example:
+      # guest_comments = guest_user.comments.all
+      # guest_comments.each do |comment|
+      # comment.user_id = current_user.id
+      # comment.save!
+      # end
     end
 
     def create_guest_user
